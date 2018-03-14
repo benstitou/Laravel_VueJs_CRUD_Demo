607bb26053ca7a4cfdec120454871a42df5fa79e
Anas BENSTITOU <anasben2013@gmail.com>
2018-03-14 14:49:10 +0000

    add modal

--- demo_crud_api_consumer/src/components/PostsManager.vue
+++ demo_crud_api_consumer/src/components/PostsManager.vue
@@ -23,6 +23,7 @@
               <td>{{ post.created_at }}</td>
               <td>{{ post.updated_at }}</td>
               <td class="text-right">
+                <a href="#" v-b-modal.myModal @click.prevent="populatePostToEdit(post)">Edit Modal</a> - 
                 <a href="#" @click.prevent="populatePostToEdit(post)">Edit</a> - 
                 <a href="#" @click.prevent="deletePost(post.id)">Delete</a>
               </td>
@@ -46,6 +47,27 @@
         </b-card>
       </b-col>
     </b-row>
+
+      <div>
+        <!-- the modal -->
+        <b-modal id="myModal" hide-footer ref="myModalRef">
+          <b-card :name="(model.id ? 'Edit Post ID#' + model.id : 'New Post')">
+            <form @submit.prevent="savePost">
+              <b-form-group label="Name">
+                <b-form-input type="text" v-model="model.name"></b-form-input>
+              </b-form-group>
+              <b-form-group label="Body">
+                <b-form-textarea rows="4" v-model="model.description"></b-form-textarea>
+              </b-form-group>
+              <div>
+                <b-btn type="submit" variant="success">Save Post</b-btn>
+              </div>
+            </form>
+          </b-card>
+        </b-modal>
+      </div>
+
+
   </div>
 </template>
 
@@ -79,6 +101,8 @@ export default {
       }
       this.model = {} // reset form
       await this.refreshPosts()
+
+      this.$refs.myModalRef.hide()
     },
     async deletePost (id) {
       if (confirm('Are you sure you want to delete this post?')) {
